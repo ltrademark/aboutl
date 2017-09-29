425b8bdaf1fad3e47d7a96a2ea6dc0de223182a2
Leo H <leo@ltrdmrk.co>
2017-09-28 20:30:36 -0400

    updates to some styles && new form
    nothing crazy

--- js/index.js
+++ js/index.js
@@ -6,6 +6,26 @@ AOS.init({
 	delay: 350,
 	disable: window.innerWidth < 786
 });
+var zi9yoyw179vy9j;
+(function(d, t) {
+	var s = d.createElement(t), options = {
+		'userName':'ltrademark',
+		'formHash':'zi9yoyw179vy9j',
+		'autoResize':true,
+		'height':'1100',
+		'async':true,
+		'host':'wufoo.com',
+		'header':'hide',
+		'ssl':true
+	};
+	s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
+	s.onload = s.onreadystatechange = function() {
+		var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+		try { zi9yoyw179vy9j = new WufooForm();zi9yoyw179vy9j.initialize(options);zi9yoyw179vy9j.display(); } catch (e) {}
+	};
+	var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
+})(document, 'script');
+
 $(window).scroll(function(){
 	var height = Math.round($('.hero').height());
 	var top = Math.round($(window).scrollTop());
@@ -15,28 +35,12 @@ $(window).scroll(function(){
 		$('.hero').find('.main-content').removeClass('fade');
 		$('body').velocity("stop");
 	}
-	// if (top > 10 && top < 100) {
-	// 	console.log('bing!');
-	// 	$('.about').velocity('scroll', {
-	// 		duration: 650,
-	// 		offset: 0,
-	// 		easing:  [0.4, 0.0, 0.2, 1]
-	// 	});
-	// 	$('.about').velocity('stop');
-	// } else if (top === (height - 20)) {
-	// 	$(document).velocity('scroll', {
-	// 		duration: 650,
-	// 		offset: 0,
-	// 		easing:  [0.4, 0.0, 0.2, 1]
-	// 	});
-	// }
-	console.log(top);
 });
-
-$('.ltm-emblem').on('click', function(){
-		$('body').velocity('scroll', {
-			duration: 650,
-			offset: 0,
-			easing:  [0.4, 0.0, 0.2, 1]
-		});
+$('#messageForm').on('click', function(){
+	$('.about').css('opacity', '0');
+	$('.message-form').addClass('open');
+});
+$('.dismiss-form').on('click', function(){
+	$('.about').css('opacity', '1');
+	$('.message-form').removeClass('open');
 });
