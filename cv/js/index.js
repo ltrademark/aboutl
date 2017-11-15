3f8b028fe8dad5cc1b068331dcfaabfe45c2442e
Leo H <leo@ltrdmrk.co>
2017-11-15 00:36:04 -0500

    sticky header on mobile now.
    still a little rough but itll work with the styles for now. finalize
    tomorrow

--- cv/js/index.js
+++ cv/js/index.js
@@ -18,7 +18,9 @@ var cv = new Vue({
             'margin-bottom: 20px',
             'font-size: 32px',
             'font-weight: bold'
-        ]
+        ],
+        scrollPosition: 0,
+        scrolling: false
     },
     methods: {
         getJobs: function () {
@@ -32,11 +34,55 @@ var cv = new Vue({
                 var styles = _this.consoleStyles.join(";");
                 console.log('%c LTRDMRK.CO', styles);
                 console.log(dateMsssg);
-                //
             });
+        },
+        resizeHeader: function (t) {
+            var start = 0;
+            var current = t;
+            var about = document.getElementsByClassName('about')[0], end = about.offsetHeight / 2;
+            // targets
+            var parent = document.getElementsByClassName('sticky-container')[0];
+            var sticky = parent.children, avatar = sticky[0], title = sticky[1], subtitle = sticky[2], details = sticky[3];
+            var avatarW = avatar.offsetWidth, subW = subtitle.offsetWidth;
+            if (this.scrolling && current < end) {
+                var inc = current * .002, move = inc - 1;
+                // avatar
+                avatar.style.transform = 'translateX(-' + current + '%) translateY(-' + current * .05 + '%) scale(' + move + ') rotate(180deg)';
+                // title
+                title.style.transform = 'translateY(-' + current * .6 + 'px) translateX(-' + current * .085 + 'px)';
+                // subheader
+                subtitle.style.transform = 'translateY(-' + current * .75 + 'px)';
+                subtitle.style.paddingLeft = current * .4 + 'px';
+                subtitle.style.textAlign = 'left';
+                // about.style.height = about.offsetHeight - current + 'px';
+                // console.log(parent.offsetHeight);
+                //details
+                var rawNum = current * .004685 - 1, fade = rawNum.toFixed(2);
+                details.style.transform = 'translateY(-' + current * .4 + '%)';
+                details.style.opacity = fade / -1;
+            }
+            else if (this.scrolling && current > end) {
+            }
+            else {
+                subtitle.style.textAlign = 'center';
+            }
+        },
+        handleScroll: function (e) {
+            var currentScrollPosition = e.target.scrollTop;
+            if (currentScrollPosition > this.scrollPosition) {
+                this.scrolling = true;
+            }
+            else {
+                this.scrolling = false;
+            }
+            this.resizeHeader(currentScrollPosition);
+            //
         }
     },
     mounted: function () {
         this.getJobs();
+        if (window.outerWidth < 800) {
+            document.getElementsByClassName('work')[0].style.marginTop = document.getElementsByClassName('about')[0].offsetHeight + 100 + 'px';
+        }
     }
 });
